1e88f3fbf5105b869f5ba8cb86229186651f5510
James Shaw <penguinoneshaw@gmail.com>
2023-08-01 17:55:18 +0100

    Even more experimental workflow

--- .github/actions/render-latex/entrypoint.sh
+++ .github/actions/render-latex/entrypoint.sh
@@ -4,12 +4,14 @@ if [ -e "extra-packages.txt" ]; then
     xargs tlmgr install <"extra-packages.txt"
 fi
 mkdir output
-output_dir=$(realpath "output")
+output_dir="output"
 
 cd "$(dirname "$1")" || exit 1
 
-latexmk -f -interaction=batchmode -pdfxe "$(basename "$1")" -output-directory="$output_dir"
+file="$(basename "$1")"
 
-latexmk -c
+latexmk -f -interaction=batchmode -pdfxe "$file" -output-directory="$output_dir"
+
+latexmk -c "$file"
 
 echo "output-dir=$output_dir" >>"$GITHUB_OUTPUT"
